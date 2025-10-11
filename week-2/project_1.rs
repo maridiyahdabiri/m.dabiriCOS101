3db3173833ced0d85de85e8810c94df6f8754e70
maridiyahdabiri <maridiyah.dabiri@edu.ng>
2025-10-11 12:05:33 +0100

    project 1

---
+++ week-2/project_1.rs
@@ -0,0 +1,12 @@
+fn main() {
+	let p:f64 - 520_000_000.00;
+	let r:f64 - 5.0;
+	let t:f64 - 10.0;
+	
+    // compound interest
+    let a=p * (1.0 + (r / 100.00)).powf (t);
+    println!("Amount is{}", a);
+    let ci = a - p;
+    println!("Compond Interest is {}", ci);
+    }
+     
