ae17debce3d4a4a6ff37ecd407350fcc644472ba
maridiyahdabiri <maridiyah.dabiri@edu.ng>
2025-11-04 00:01:26 +0100

    (practice_8)

---
+++ week-6/practice_8/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let num1 = 10.0;
+    let num2 = 2.0;
+    let mut resut:i32;
+
+   let result = num1 + num2;
+    println!("Sum: {}",result);
+
+   let result = num1 - num2;
+    println!("Difference: {}",result);
+
+   let result = num1*num2;
+    println!("Product: {}",result);
+
+   let result = num1/num2;
+    println!("Quotient: {}",result);
+
+   let result = num1%num2;
+    println!("Remainder: {}",result);
+}    
---
+++ week-6/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\Documents\PAUClasses\100LV\COS101\m.dabiriCOS101\week-6\practice_8\target\debug\practice_8.exe: C:\Users\LENOVO\Documents\PAUClasses\100LV\COS101\m.dabiriCOS101\week-6\practice_8\src\main.rs
