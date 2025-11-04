113564fce68c80a1b3e3941a60fb293285f54a0a
maridiyahdabiri <maridiyah.dabiri@edu.ng>
2025-11-04 05:07:53 +0100

    (practice_10)

---
+++ week-6/practice_10/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+   let a = 20.0;
+   let b = 30.0;
+
+if (a > 10.0) &&(b > 10.0) {
+    println!("true");
+   }
+   let c = 0.0;
+   let d = 30.0;
+
+   if (c > 10.0) || (d > 10.0) {
+    println!("true");
+   }
+   let is_elder = false;
+
+   if !is_elder 
+   {
+    println!("Not Elder");
+   }
+}   
