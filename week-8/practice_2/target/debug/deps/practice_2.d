d1271d8ecd69514d11cb355c480f689eb226ca32
maridiyahdabiri <maridiyah.dabiri@edu.ng>
2025-11-17 11:37:05 +0100

    (practice_2)

---
+++ week-8/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Documents\PAUClasses\100LV\COS101\m.dabiriCOS101\week-8\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\LENOVO\Documents\PAUClasses\100LV\COS101\m.dabiriCOS101\week-8\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
